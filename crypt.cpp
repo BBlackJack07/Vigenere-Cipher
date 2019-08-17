2788f735564b4214d2be168b0280e06410224b23
bblackjack07 <bastien.brunel.bb@gmail.com>
2019-08-17 16:57:55 +0200

    use arguments in crypt.cpp and decrypt.cpp

--- crypt.cpp
+++ crypt.cpp
@@ -2,20 +2,29 @@
 #include <thread>
 #include "vigenere.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
     
-	using namespace std::chrono_literals;
-    std::string str {};
-    std::string key {};
-    std::cout << "Entrez la clé : ";
-    getline(std::cin, key);
-    std::this_thread::sleep_for(1000ms);
-    std::cout << "Entrez la phrase à chiffrer : ";
-    getline(std::cin, str);
+    if (argc == 3)
+    {
+        std::string msg = argv[1];
+        std::string key = argv[2];
+        std::cout << Vigenere::encrypt(msg, key) << std::endl;
+    }
 
-    std::cout << std::endl << Vigenere::encrypt(str, key) << std::endl;
-    
+    else
+    {
+	    using namespace std::chrono_literals;
+        std::string str {};
+        std::string key {};
+        std::cout << "Entrez la clé : ";
+        getline(std::cin, key);
+        std::this_thread::sleep_for(1000ms);
+        std::cout << "Entrez la phrase à chiffrer : ";
+        getline(std::cin, str);
+
+        std::cout << std::endl << Vigenere::encrypt(str, key) << std::endl;
+    }
     return 0;
 
 }
