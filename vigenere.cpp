c12dcd864ea86fe159c1b363a373cd7438078043
bblackjack07 <bastien.brunel.bb@gmail.com>
2019-08-23 11:19:14 +0200

    Use one object 'VigenereCipher'

--- vigenere.cpp
+++ vigenere.cpp
@@ -2,78 +2,60 @@
 
 using namespace Vigenere;
 
-VigenereEncrypt::VigenereEncrypt(std::string key)
+VigenereCipher::VigenereCipher(std::string key, std::string mode)
 {
     m_key = key;
     m_count = 0;
+
+    if(mode == "encrypt") 
+        m_mode = true;
+    else if (mode == "decrypt") 
+        m_mode = false;
+    else 
+        throw "Mode '" + mode + "' doesn't exist!";
 }
 
-
-VigenereDecrypt::VigenereDecrypt(std::string key)
-{
-    m_key = key;
-    m_count = 0;
-}
-
-int Vigenere::index(char c)
+int VigenereCipher::index(char c)
 {
     for (unsigned short int i {0}; i < AVALAIBLE_CHARS.size(); i++)
         if (c == AVALAIBLE_CHARS[i]) { return i; }
     return -1;
 }
 
-char VigenereEncrypt::operator()(const char &char_to_encrypt)
+char VigenereCipher::operator()(const char &char_to_encrypt)
 {
     char c {char_to_encrypt};
     char r {};
-    unsigned int shift {};
-    if (isalnum(c) || c == ' ')
-    {
-        if (m_count < m_key.size())
-            shift=index(m_key[m_count]);  
-        else
-            shift=index(m_key[m_count%m_key.size()]);
-
-        if (shift+index(c) < AVALAIBLE_CHARS.size())
-            r=AVALAIBLE_CHARS[shift+index(c)];
-        else
-            r=AVALAIBLE_CHARS[shift+index(c)-AVALAIBLE_CHARS.size()];
-            
-    }
-    else
-        r=c;
-    m_count++;
-    return r;
-}
-
-char VigenereDecrypt::operator()(const char &char_to_decrypt)
-{
-    char c {char_to_decrypt};
-    char r {};
     int shift {};
+    
     if (isalnum(c) || c == ' ')
     {
-        if (m_count < m_key.size())
-            shift=index(m_key[m_count]);  
-        else
-            shift=index(m_key[m_count%m_key.size()]);
+        shift=index(m_key[m_count%m_key.size()]);
 
-        if (index(c)-shift >= 0 )
-            r=AVALAIBLE_CHARS[index(c)-shift];
+        if(m_mode)
+        {
+            r=AVALAIBLE_CHARS[(index(c) + shift) % AVALAIBLE_CHARS.size()];
+        }
+        
         else
-            r=AVALAIBLE_CHARS[index(c)-shift+AVALAIBLE_CHARS.size()];
-            
+        {
+            if (index(c)-shift >= 0 )
+                r=AVALAIBLE_CHARS[index(c)-shift];
+            else
+                r=AVALAIBLE_CHARS[index(c)-shift+AVALAIBLE_CHARS.size()];
+        }
     }
+
     else
         r=c;
+    
     m_count++;
     return r;
 }
-
 
 std::string Vigenere::encrypt(std::string msg, std::string key)
 {
-    VigenereEncrypt vencrypt(key);
+    VigenereCipher vencrypt(key, "encrypt");
     const int msgLen = msg.size();
     std::string vigenere_msg(msgLen, ' ');
     std::transform(msg.begin(), msg.end(), vigenere_msg.begin(), vencrypt);
@@ -83,7 +65,7 @@ std::string Vigenere::encrypt(std::string msg, std::string key)
 
 std::string Vigenere::decrypt(std::string vigenere_msg, std::string key)
 {
-    VigenereDecrypt vdecrypt(key);
+    VigenereCipher vdecrypt(key, "decrypt");
     const int vmsgLen = vigenere_msg.size();
     std::string msg(vmsgLen, ' ');
     std::transform(vigenere_msg.begin(), vigenere_msg.end(), msg.begin(), vdecrypt);
--- vigenere.hpp
+++ vigenere.hpp
@@ -5,33 +5,20 @@ namespace Vigenere
 {
     const std::string AVALAIBLE_CHARS { "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 " };
 
-    int index(char c);
-
     std::string encrypt(std::string msg, std::string key);
     std::string decrypt(std::string vigenere_msg, std::string key);
 
-    class VigenereEncrypt
+    class VigenereCipher
     {
         public:
-            VigenereEncrypt(std::string key);
+            VigenereCipher(std::string key, std::string mode);
             char operator()(const char &char_to_encrypt);
-    
+            int index(char c);
+
         private:
             unsigned int m_count;
             std::string m_key;
-
-    };
-
-    class VigenereDecrypt
-    {
-        public:
-            VigenereDecrypt(std::string key);
-            char operator()(const char &char_to_decrypt);
-    
-        private:
-            unsigned int m_count;
-            std::string m_key;
-
+            bool m_mode;
     };
 
 }
